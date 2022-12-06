7050b00ab2242ae06d17bfc9b803f4dc9b9bef57
Allen Mordukhaev <allenmor93@gmail.com>
2022-12-06 11:43:21 -0500

    12/6

--- App.js
+++ App.js
@@ -5,37 +5,58 @@ import {
   Button,
   TextInput,
   ScrollView,
-  FlatList
+  FlatList,
+  Pressable,
 } from "react-native";
 import { useState } from "react";
 import GoalInput from "./components/GoalInput";
 import GoalItem from "./components/GoalItem";
 
-
-
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
   function deleteGoalHandler(id) {
-    setCourseGoals(prev => prev.filter((el, i) => {
-      return el.id !== id
-    }))
+    setCourseGoals((prev) =>
+      prev.filter((el, i) => {
+        return el.id !== id;
+      })
+    );
+  }
 
+  function handleNBAClick() {
+    setCourseGoals([...courseGoals, {text: 'NBA', id: `${Math.random()}`}])
+  }
+  function handleResetClick(){
+    setCourseGoals([])
   }
   return (
     <View style={styles.appContainer}>
-      <View>
-        <Text>NBA Report</Text>
-      </View>
+      <Pressable onPress={handleNBAClick}>
+        <View style={styles.nbaLogo}>
+          <Text style={styles.nbaText}>NBA Report</Text>
+        </View>
+      </Pressable>
       <GoalInput setCourseGoals={setCourseGoals} />
+      <Pressable onPress={handleResetClick}>
+      <View style={styles.resetBtn}>
+        <Text style={styles.resetText}>Reset</Text>
+      </View>
+      </Pressable>
       <View style={styles.goalsContainer}>
-        <FlatList data={courseGoals} renderItem={(itemData => {
-          itemData.index
-          return (
-            <GoalItem deleteGoalHandler={deleteGoalHandler} text={itemData.item.text} id={itemData.item.id} />
-          )
-        })} alwaysBounceVertical={false}>
-        </FlatList>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            itemData.index;
+            return (
+              <GoalItem
+                deleteGoalHandler={deleteGoalHandler}
+                text={itemData.item.text}
+                id={itemData.item.id}
+              />
+            );
+          }}
+          alwaysBounceVertical={false}
+        ></FlatList>
       </View>
     </View>
   );
@@ -49,5 +70,27 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  nbaLogo: {
+    backgroundColor: "red",
+    borderRadius: 10,
+  },
+  nbaText: {
+    fontWeight: "bold",
+    fontSize: 30,
+    padding: 10,
+  },
+  resetBtn: {
+    borderBottomWidth: 1,
+    borderBottomColor: "gray",
+    padding: 4,
+    backgroundColor: 'black',
+    borderRadius: 8,
+    marginBottom: 5
+  },
+  resetText: {
+    color: 'white',
+    fontWeight: 'bold',
+    padding: 10
   }
 });
